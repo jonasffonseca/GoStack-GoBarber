536466943fe35c217202db5cb2a627b148b1d49d
Jonas Fonseca <jonas.fragafonseca@gmail.commit>
2020-12-25 22:01:59 -0300

    Create Profile Controller

--- src/modules/users/infra/http/middlewares/ensureAuthenticated.ts
+++ src/modules/users/infra/http/middlewares/ensureAuthenticated.ts
@@ -3,7 +3,7 @@ import { verify } from 'jsonwebtoken';
 import authConfig from '@config/auth';
 import AppError from '@shared/errors/AppError';
 
-interface TokenPayload {
+interface ITokenPayload {
   iat: number;
   exp: number;
   sub: string;
@@ -22,7 +22,7 @@ export default function ensureAuthenticated(
   const { secret } = authConfig.jwt;
   try {
     const decoded = verify(token, secret);
-    const { sub } = decoded as TokenPayload;
+    const { sub } = decoded as ITokenPayload;
 
     request.user = {
       id: sub,
