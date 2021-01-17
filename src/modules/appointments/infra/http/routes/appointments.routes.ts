22b049bb22104712876fa7fc6260e8dcb1ae4f3f
Jonas Fonseca <jonas.fragafonseca@gmail.commit>
2021-01-17 18:57:02 -0300

    add Provider Schedule List Service

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,17 +1,13 @@
 import { Router } from 'express';
 import AppointmentsController from '../controllers/AppointmentsController';
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 const appointmentsRouter = Router();
 const appointmentsController = new AppointmentsController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
-// appointmentsRouter.get('/', async (request, response) => {
-//   console.log(request.user);
-// const appointmentsRepository = container.resolve(AppointmentsRepository);
-
-//   const appointments = await appointmentsRepository.find();
-//   return response.json(appointments);
-// });
 
 appointmentsRouter.post('/', appointmentsController.create);
+appointmentsRouter.get('/me', providerAppointmentsController.index);
 export default appointmentsRouter;
