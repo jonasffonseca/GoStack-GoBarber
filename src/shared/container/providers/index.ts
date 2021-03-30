3face1af905f67e9e07510f62b7986a2f7ba13f0
Jonas Fonseca <jonas.fragafonseca@gmail.commit>
2021-03-29 23:58:53 -0300

    Refactoring Providers Container

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,26 +1,3 @@
-import { container } from 'tsyringe';
-import mailConfig from '@config/mail';
-
-import IStorageProvider from '../providers/StorageProvider/models/IStorageProvider';
-import DiskStorageProvider from '../providers/StorageProvider/implementations/DiskStorageProvider';
-import IMailProvider from '../providers/MailProvider/models/IMailProvider';
-import EtherealMailProvider from '../providers/MailProvider/implementations/EtherealMailProvider';
-import SESMailProvider from '../providers/MailProvider/implementations/SESMailProvider';
-import IMailTemplateProvider from '../providers/MailTemplateProvider/models/IMailTemplateProvider';
-import HandlebarsMailTemplateProvider from '../providers/MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
-
-container.registerSingleton<IStorageProvider>(
-  'StorageProvider',
-  DiskStorageProvider,
-);
-container.registerSingleton<IMailTemplateProvider>(
-  'MailTemplateProvider',
-  HandlebarsMailTemplateProvider,
-);
-
-container.registerInstance<IMailProvider>(
-  'MailProvider',
-  mailConfig.driver === 'ethereal'
-    ? container.resolve(EtherealMailProvider)
-    : container.resolve(SESMailProvider),
-);
+import './StorageProvider';
+import './MailTemplateProvider';
+import './MailProvider';
