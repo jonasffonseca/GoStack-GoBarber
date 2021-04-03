f7c59dfbee22f9058804d4792b354bff9685ea4a
Jonas Fonseca <jonas.fragafonseca@gmail.commit>
2021-04-02 21:11:27 -0300

    Fix - Add user info on get appointments from provider by day

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -54,6 +54,7 @@ class AppointmentsRepository implements IAppointmentsRepository {
             `to_char(${dateFieldName},'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`,
         ),
       },
+      relations: ['user'],
     });
     return appointments;
   }
