9603e6fad60a5448d385f64d2837b834e24a997b
Jonas Fonseca <jonas.fragafonseca@gmail.commit>
2021-04-08 19:07:58 -0300

    Dashboard finished

--- src/hooks/AuthContext.tsx
+++ src/hooks/AuthContext.tsx
@@ -31,6 +31,7 @@ export const AuthProvider: React.FC = ({ children }) => {
     const token = localStorage.getItem("@GoBarber:token");
     const user = localStorage.getItem("@GoBarber:user");
     if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
       return { token, user: JSON.parse(user) };
     }
     return {} as AuthState;
@@ -44,6 +45,7 @@ export const AuthProvider: React.FC = ({ children }) => {
     localStorage.setItem("@GoBarber:token", token);
     localStorage.setItem("@GoBarber:name", user.name);
     localStorage.setItem("@GoBarber:user", JSON.stringify(user));
+
     setData({ token, user });
   }, []);
   const signOut = useCallback(() => {
